9fc9fbb929a7debc94b94ea36dccc5cbd128fc93
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-12-13 23:40:10 +0100

    add species lists

--- R/11_Acoustic_analysis.R
+++ R/11_Acoustic_analysis.R
@@ -768,7 +768,8 @@ species_list_deployment <-
     median_confidence = median(confidence, na.rm = TRUE),
     min_confidence = min(confidence, na.rm = TRUE),
     max_confidence = max(confidence, na.rm = TRUE),
-    sd_confidence = sd(confidence, na.rm = TRUE)
+    sd_confidence = sd(confidence, na.rm = TRUE),
+    n_recordings = n()
     )
 
 species_list_habitat <- 
@@ -779,8 +780,31 @@ species_list_habitat <-
     median_confidence = median(confidence, na.rm = TRUE),
     min_confidence = min(confidence, na.rm = TRUE),
     max_confidence = max(confidence, na.rm = TRUE),
-    sd_confidence = sd(confidence, na.rm = TRUE)
+    sd_confidence = sd(confidence, na.rm = TRUE),
+    n_recordings = n()
   )
+
+# ----------------------------------------------------------------- #
+### 1. Save all summaries as CSV files ----
+# ----------------------------------------------------------------- #
+file_species_list_deployment <- "Outputs/Results/species_list_deployment"
+file_species_list_habitat <- "Outputs/Results/species_list_habitat"
+
+message("--- Saving CSV files ---")
+
+# Save the species_list_deployment
+readr::write_csv(
+  species_list_deployment,
+  paste0(file_species_list_deployment, ".csv")
+)
+message("Saved: ", paste0(file_species_list_deployment, ".csv"))
+
+# Save the species_list_habitat
+readr::write_csv(
+  species_list_habitat,
+  paste0(file_species_list_habitat, ".csv")
+)
+message("Saved: ", paste0(file_species_list_habitat, ".csv"))
 
 # ----------------------------------------------------------------- #
 ## Summary Table by Group (e.g., deployment) ----
